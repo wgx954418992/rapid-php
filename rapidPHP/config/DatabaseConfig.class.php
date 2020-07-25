2bc8de4ff859c0a8640e61149f50257d8e5d743c
王更新 <wgx954418992@qq.com>
2020-07-26 02:50:52 +0800

    delete : Please enter the commit message for your changes. Lines starting

--- rapidPHP/config/DatabaseConfig.class.php
+++ rapidPHP/config/DatabaseConfig.class.php
@@ -15,11 +15,11 @@ class DatabaseConfig
     public static $default = [
         'string' => '',//连接字符串(为空则按照下面配置连接)(mysql:host=127.0.0.1;dbname=test)
         'username' => 'root',//用户名
-        'password' => 'Wgx954418992',//密码
+        'password' => 'root',//密码
         'header' => 'host',//连接头 (host|Server)
-        'host' => 'rm-bp1un7cx4r5c28jj9yo.mysql.rds.aliyuncs.com',//地址
+        'host' => '127.0.0.1',//地址
         'driver' => Mysql::class,//驱动
-        'database' => 'iot-core',//数据库
+        'database' => 'test',//数据库
         'databaseType' => 'mysql',//数据库类型(sqlsrv|mysql|...)
         'databaseConnectName' => 'dbname',//数据库连接名称(sqlsrv:database,mysql:dbname,...)
         'databaseCode' => 'utf8mb4',//数据库编码
--- rapidPHP/config/RedisConfig.class.php
+++ rapidPHP/config/RedisConfig.class.php
@@ -9,7 +9,7 @@ class RedisConfig
      * redis 地址
      * @var string
      */
-    public static $host = '47.97.75.148';
+    public static $host = '127.0.0.1';
 
     /**
      * redis 端口
@@ -21,7 +21,7 @@ class RedisConfig
      * redis 密码
      * @var string|null
      */
-    public static $auth = 'Wgx954418992';
+    public static $auth = '';
 
     /**
      * 数据库 默认0
