98be66eb1b333532a411bb4178ad5794eb3466b5
王更新 <wgx954418992@qq.com>
2021-08-17 15:37:21 +0800

    fix bug

--- apps/admin/public/websocket.php
+++ apps/admin/public/websocket.php
@@ -3,7 +3,7 @@
 use rapidPHP\modules\application\classier\apps\SwooleWebsocketApplication;
 use rapidPHP\modules\configure\classier\Configurator;
 
-define('EVN_NAME', empty(get_cfg_var('env.name')) ? get_cfg_var('env.name') : 'dev');
+define('ENV_NAME', !empty(get_cfg_var('env.name')) ? get_cfg_var('env.name') : 'dev');
 
 define('PATH_APP', str_replace('\\', '/', dirname(__DIR__)) . '/');
 
@@ -17,6 +17,7 @@ $app = new SwooleWebsocketApplication(Configurator::getInstance());
 $app->getConfig()
     ->setPaths([
         PATH_ROOT . 'apps/core/application.yaml',
+        PATH_ROOT . 'apps/core/config/' . ENV_NAME,
         PATH_APP . 'application.yaml'
     ])
     ->load();
--- apps/app/public/http.php
+++ apps/app/public/http.php
@@ -3,7 +3,7 @@
 use rapidPHP\modules\application\classier\apps\SwooleHttpApplication;
 use rapidPHP\modules\configure\classier\Configurator;
 
-define('EVN_NAME', empty(get_cfg_var('env.name')) ? get_cfg_var('env.name') : 'dev');
+define('ENV_NAME', !empty(get_cfg_var('env.name')) ? get_cfg_var('env.name') : 'dev');
 
 define('PATH_APP', str_replace('\\', '/', dirname(__DIR__)) . '/');
 
@@ -17,7 +17,7 @@ $app = new SwooleHttpApplication(Configurator::getInstance());
 $app->getConfig()
     ->setPaths([
         PATH_ROOT . 'apps/core/application.yaml',
-        PATH_ROOT . 'apps/core/config/' . EVN_NAME,
+        PATH_ROOT . 'apps/core/config/' . ENV_NAME,
         PATH_APP . 'application.yaml'
     ])
     ->load();
